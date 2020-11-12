5f5390bbcf6968d1a83cfce78d6348e5f0e924f0
slayingripper <kirav96@gmail.com>
2020-11-13 00:39:05 +0200

    new file

---
+++ pypacket/util/Packetreader.py
@@ -0,0 +1,10 @@
+def packetexecute(packetstring):
+    
+    if '#273#' in packetstring:
+        print("Packet found")
+    elif 'Nicosia' in packetstring:
+        print("Packet found")
+
+    
+if __name__ == "__main__":
+    packetexecute("")
